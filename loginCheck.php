374af7d5993498c7748bb3c517b70a42f4e06ee0
Mark Furland <markfurland@gmail.com>
2014-09-06 09:14:27 +0000

    Login now with sketchy cookies

--- loginCheck.php
+++ loginCheck.php
@@ -1,27 +1,36 @@
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-?>
-<?PHP
 
 include('db.php');
 
 $dbh = connectDB();
 
 $name = $_POST["name"];
-echo $name;
-echo '<br />';
+//echo $name;
+//echo '<br />';
 $email = $_POST["email"];
-echo $email;
+//echo $email;
 
 
-$stmt = $dbh->prepare("SELECT * FROM users WHERE email = :email AND name = :name");
-$stmt->execute(array('email' => $email, 'name' => $name));
+$stmt = $dbh->prepare("SELECT * FROM users WHERE email = :email");
+$stmt->execute(array('email' => $email));
 
-echo "<pre>";
-print_r($stmt->rowCount());
-echo "</pre>";
+//echo "<br />";
+//echo $stmt->rowCount();
 
+if ($stmt->rowCount() == 0) {
+	$stmt = $dbh->prepare("INSERT INTO users (email,name) value (:email, :name)");
+	$stmt->execute(array('email' => $email, 'name' => $name));
+	//echo "<br />Not dead yet";
+	$stmt = $dbh->prepare("SELECT * FROM users WHERE email = :email");
+	$stmt->execute(array('email' => $email));
+	//echo "<br />Reselected";
+}
+
+$row = $stmt->fetch();
+//print_r( $row );
+setcookie("User",$row[0],time()+60*60*24);
 
 
 ?>
