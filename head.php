663231ad9383386c4c49a809fe8e7fd253eb2dfc
Mark Furland <markfurland@gmail.com>
2014-09-06 08:52:59 +0000

    Updated head/foot
    Now in their own files, and validates
    Also, bootstrap exists

---
+++ head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?PHP echo $title; ?></title>
+		
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" href="css.css">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
--- index.php
+++ index.php
@@ -3,43 +3,48 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 require_once("db.php");
-?>
-<html>
-	<head>
-		<title>TODO: Name this shit</title>
-	</head>
-	<body>
-<?PHP
-		$action = isset($_GET['action']) ? $_GET['action'] : null;
-		switch($action){
-			case 'insert':
-				$insertType = isset($_GET['insertType']) ? $_GET['insertType'] : null;
-				if($insertType == "container"){
-					echo "Inserting container<br>";
-					$user = isset($_GET['user']) ? $_GET['user'] : null;
-					$name = isset($_GET['name']) ? $_GET['name'] : null;
-					$location = isset($_GET['location']) ? $_GET['location'] : null;
-					$success = insertContainer($user,$name,$location);
-				}elseif($insertType == "object"){
-					echo "Inserting object<br>";
-					$user = isset($_GET['user']) ? $_GET['user'] : null;
-					$container = isset($_GET['container']) ? $_GET['container'] : null;
-					$name = isset($_GET['name']) ? $_GET['name'] : null;
-					$keywords = isset($_GET['keywords']) ? $_GET['keywords'] : null;
-					$success = insertObject($user,$container,$name,$keywords);
-				}else{
-					die("Bad insert type");
-				}
-				if($success){
-					echo "successfully added";
-				}else{
-					echo "failure";
-				}
-				break;
-			default:
-				echo "no action";	
+
+
+$title = "TODO: Title shit";
+include('head.php');
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+switch($action){
+	case 'insert':
+		$insertType = isset($_GET['insertType']) ? $_GET['insertType'] : null;
+		if($insertType == "container"){
+			echo "Inserting container<br>";
+			$user = isset($_GET['user']) ? $_GET['user'] : null;
+			$name = isset($_GET['name']) ? $_GET['name'] : null;
+			$location = isset($_GET['location']) ? $_GET['location'] : null;
+			$success = insertContainer($user,$name,$location);
+		}elseif($insertType == "object"){
+			echo "Inserting object<br>";
+			$user = isset($_GET['user']) ? $_GET['user'] : null;
+			$container = isset($_GET['container']) ? $_GET['container'] : null;
+			$name = isset($_GET['name']) ? $_GET['name'] : null;
+			$keywords = isset($_GET['keywords']) ? $_GET['keywords'] : null;
+			$success = insertObject($user,$container,$name,$keywords);
+		}else{
+			die("Bad insert type");
 		}
-		
+		if($success){
+			echo "successfully added";
+		}else{
+			echo "failure";
+		}
+		break;
+	default:
+		echo "no action";	
+}
 ?>
-	</body>
-</html>
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+<?PHP
+include('foot.php');
