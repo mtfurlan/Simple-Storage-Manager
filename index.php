e4776e500f08a395e5f5f36fc93c0f6758091254
Mark Furland <markfurland@gmail.com>
2014-09-06 21:49:09 +0000

    Landing page text updates

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ include('nav.php');
 
       <div class="starter-template">
         <h1>Simple Inventory Manager</h1>
-
+		<br /><br />
         <p class="lead">
-		This object storage tracking system was created to aid in the controlled storage of all the random different things one accumulates over time. Where did that cool switch found in the physics department trash go? Where did the wirestrippers go? With this, you can keep a bit closer control over where you put your things.
+		This object storage tracking system was created to aid in the controlled storage of all the random different things one accumulates over time. Where did that cool switch found in the physics department trash go? Where did the wirestrippers go? With SIM, you can keep a bit closer control over where your things go.
 <br><br>
-This helps you maintain a list of containers such as drawers, boxes, shelves, etc, and lists of items in or on those containers. You can print lables for the containers, with both a human readable ID, and a QR code that allow you to quickly get a list of items in that container. You can also search for an item, and a list of possible items will come up, and tell you what containers they are in.
-<br>
-Accounts are easily created via a google login, and your containers/objecs are kept seperate from other accounts.
-<br>
-Android app coming soon, the API for such an app to connect to already exists.
+This helps you maintain a list of containers such as drawers, boxes, closets, etc, and lists of all the important items in or on those containers. You can print labels for the containers, with a human readable ID, a timestamp, and a QR code that will allow you to quickly pull up a list of the contents with your smartphone. You can also search for items or boxes and the built in tracking system will help you quickly locate them.
+<br><br>
+Accounts are easily created using your existing gmail account, allowing for easy one-click sign-in.
+<br><br>
+API and Android app coming soon.
 		<!--<div class="startButton"><a href='#'>A simple answer to a simple question:</a></div>-->
 	</p>
       </div>
